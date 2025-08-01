de98fa99c684c0f4efb744cc9d06af4f2810e28e
insertusernamed <113151220+insertusernamed@users.noreply.github.com>
2025-07-31 22:57:11 -0400

    initial commit

---
+++ src/main/java/org/insertusernamed/themotherplant/ThemotherplantApplication.java
@@ -0,0 +1,13 @@
+package org.insertusernamed.themotherplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemotherplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemotherplantApplication.class, args);
+	}
+
+}
