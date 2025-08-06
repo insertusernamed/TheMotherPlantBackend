93f4e59dc299e089ab316382952210fd16cdfe64
insertusernamed <113151220+insertusernamed@users.noreply.github.com>
2025-08-06 13:08:37 -0400

    updated CORS config

--- src/main/java/org/insertusernamed/themotherplant/config/SecurityConfig.java
+++ src/main/java/org/insertusernamed/themotherplant/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(List.of(
 				"http://localhost:5173",
-				"mother-plant.vercel.app"
+				"https://mother-plant.vercel.app"
 		));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
